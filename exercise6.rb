3e31693868113fd2a161fbd098916b6105f77bb3
Craig Franklin <craigfranklin@Craigs-MacBook-Pro-2.local>
2015-06-10 16:33:26 -0400

    Finalized exercise6.

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,26 @@
 grocery_list = ["chicken", "tacos", "salad", "apples", "lemon"]
 
-print "* "
-
-puts grocery_list.join("\n* ")
+def checklist(list) 
+	print " * "
+	puts "#{list.join("\n * ")}\n\n" 
+end
 
 grocery_list << "rice"
+
+checklist grocery_list
+
+puts grocery_list.length
+
+if grocery_list.include? "bananas" == false
+	puts "You need to pick up bananas!"
+else 
+	puts "You don't need to pick up bananas today, silly!"
+end
+
+puts grocery_list[1]
+
+puts checklist grocery_list.sort
+
+grocery_list.delete_at(2) #Didn't include salmon, replaced with salad!
+
+puts checklist grocery_list
