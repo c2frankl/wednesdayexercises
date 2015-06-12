5d1d0c53445ea9a96c05b19bc27e7039811f8f01
Craig Franklin <craigfranklin@Craigs-MacBook-Pro-2.local>
2015-06-11 21:36:48 -0400

    Deleted second cohort, and  worked out the total amount of students.

--- exercise7.rb
+++ exercise7.rb
@@ -5,9 +5,36 @@ students = {
 }
 
 def cohort(hash)
-hash.each do |key, value|
-  puts "#{key}: #{value} students"
+  hash.each do |key, value|
+    puts "#{key}: #{value} students"
+  end
+    puts "\n"
 end
+
+cohort students
+
+students[:cohort4] = 43
+
+cohort students
+
+def cohort2(hash)
+hash.each do |key, value|
+  puts "#{key}: #{(value.to_i) * 1.05} students"
+end 
+  puts "\n"
+end
+
+cohort2 students
+
+students.delete(:cohort2)
+
+cohort2 students
+
+total = 0
+students.each do |key, value|
+  total += value
+end
+puts "#{total} is the total amount of students"
 
 # The classrooms have been expanded: increase each cohort number by 5% and display the new results.
 # Delete the 2nd cohort, and redisplay the cohorts
